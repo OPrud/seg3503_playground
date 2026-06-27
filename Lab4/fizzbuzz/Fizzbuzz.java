45c01592c919517b81fefee993187698e8da62e4
Nina Ukoha <nukoh033@uottawa.ca>
2026-06-27 15:46:47 -0400

    Add Buzz rule (GREEN). JUnit: 3 tests, 0 failed

--- Lab4/fizzbuzz/Fizzbuzz.java
+++ Lab4/fizzbuzz/Fizzbuzz.java
@@ -3,6 +3,7 @@ package fizzbuzz;
 public class Fizzbuzz {
     public static String fizzbuzz(int n) {
         if (n % 3 == 0) return "Fizz";
+        if (n % 5 == 0) return "Buzz";
         return String.valueOf(n);
     }
 }
