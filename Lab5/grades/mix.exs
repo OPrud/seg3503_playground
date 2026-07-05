54c3d719b01b010c0e30517a527161bf149cf734
Nina Ukoha <nukoh033@uottawa.ca>
2026-07-05 14:04:55 -0400

    Fix cowlib dependency; observe Calculator crash before changes (page 9 of lab slides)

--- Lab5/grades/mix.exs
+++ Lab5/grades/mix.exs
@@ -41,7 +41,8 @@ defmodule Grades.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:cowlib, "~> 2.13", override: true}
     ]
   end
 
